5b44d35275975f5b75e2180422ceb00fb4f9368e
Kush Pandya <kushpandya111@gmail.com>
2019-08-20 11:48:23 +0530

    All set balls, score, sounds, activities...

---
+++ app/src/main/java/theblackdiamonds/com/SoundPlayer.java
@@ -0,0 +1,43 @@
+package theblackdiamonds.com;
+
+import android.content.Context;
+import android.media.AudioAttributes;
+import android.media.AudioManager;
+import android.media.SoundPool;
+import android.os.Build;
+
+class SoundPlayer {
+
+    private static SoundPool soundPool;
+    private static int hitSound;
+    private static int overSound;
+
+    SoundPlayer(Context context) {
+        int SOUND_POOL_MAX = 2;
+        if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.LOLLIPOP) {
+            AudioAttributes audioAttributes = new AudioAttributes.Builder()
+                    .setUsage(AudioAttributes.USAGE_GAME)
+                    .setContentType(AudioAttributes.CONTENT_TYPE_MUSIC)
+                    .build();
+
+            soundPool = new SoundPool.Builder()
+                    .setAudioAttributes(audioAttributes)
+                    .setMaxStreams(SOUND_POOL_MAX)
+                    .build();
+        } else {
+            soundPool = new SoundPool(SOUND_POOL_MAX, AudioManager.STREAM_MUSIC, 0);
+        }
+
+        hitSound = soundPool.load(context, R.raw.hit, 1);
+        overSound = soundPool.load(context, R.raw.over, 1);
+    }
+
+    void playHitSound() {
+        soundPool.play(hitSound, 1.0f, 1.0f, 1, 0, 1.0f);
+    }
+
+    void playOverSound() {
+        soundPool.play(overSound, 1.0f, 1.0f, 1, 0, 1.0f);
+    }
+
+}
